3f64d3c59c017a7cf05eca26b371f43c5d578247
lauriemcgee <livingoncoffeeandflowers@gmail.com>
2017-04-04 11:16:50 -0500

    add Faker data

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -3,11 +3,44 @@ class CharactersController < ApplicationController
     @characters = Character.all
     render "index.json.jbuilder"
   end
+  def create
+    @character = Character.new(
+      character_name: = params[:character_name],
+      class: = params[:class],
+      level: = params[:level],
+      background: = params[:background],
+      player_name: = params[:player_name],
+      race: = params[:race],
+      alignment: = params[:alignment],
+      experience_points: = params[:experience_points],
+      strength: = params[:strength],
+      dexterity: = params[:dexterity],
+      constitution: = params[:constitution],
+      intelligence: = params[:intelligence],
+      wisdom: = params[:wisdom],
+      charisma: = params[:charisma],
+      proficiency_bonus: = params[:proficiency_bonus],
+      armor_class: = params[:armor_class],
+      initiative: = params[:initiative],
+      speed: = params[:speed],
+      current_hit_points: = params[:current_hit_points],
+      temporary_hit_points: = params[:temporary_hit_points],
+      hit_dice: = params[:hit_dice],
+      personality_traits: = params[:personality_traits],
+      ideals: = params[:ideals],
+      bonds: = params[:bonds],
+      flaws: = params[:flaws],
+      features_and_traits: = params[:features_and_traits],
+      attacks_and_spellcasting: = params[:attacks_and_spellcasting],
+      equipment: = params[:equipment],
+      other_proficiencies_and_languages: = params[:other_proficiencies_and_languages]
+      )
+    @character.save
+    redirect_to "/characters/#{@character.id}"
+  end
   def show
     @character = Character.find_by(id: params[:id])
     render "show.json.jbuilder"
-  end
-  def create
   end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,35 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+50.times do
+  Character.create( 
+  character_name: Faker::GameOfThrones.character
+  class: Faker::RickAndMorty.location
+  level: Faker::Number.number(1)
+  background: Faker::Demographic.race 
+  player_name: Faker::Name.first_name
+  race: Faker::Cat.breed
+  alignment: Faker::StarWars.specie
+  experience_points: Faker::Number.number(1)
+  strength: Faker::Number.number(2)
+  dexterity: Faker::Number.number(2)
+  constitution: Faker::Number.number(2)
+  intelligence: Faker::Number.number(2)
+  wisdom: Faker::Number.number(2)
+  charisma: Faker::Number.number(2)
+  proficiency_bonus: Faker::Number(1)
+  armor_class: Faker::Number.number(2)
+  initiative: Faker::Number.number(2)
+  speed: Faker::Number.number(2)
+  current_hit_points: Faker::Number.number(2)
+  temporary_hit_points: Faker::Number.number(2)
+  hit_dice: 1d8
+  personality_traits: Faker::StarWars.quote
+  ideals: Faker::HarryPotter.quote
+  bonds: Faker::HarryPotter.quote
+  flaws: Faker::HarryPotter.quote
+  features_and_traits: Faker::Superhero.power
+  attacks_and_spellcasting: Faker::Superhero.power
+  equipment: Faker::StarWars.vehicle
+  other_proficiencies_and_languages: Faker::Demographic.educational_attainment
+  )
+end
+
+puts "Done!"
