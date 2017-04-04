7710d5596b44a69cfde8c1667b20fdfc6c48a586
lauriemcgee <livingoncoffeeandflowers@gmail.com>
2017-04-03 21:26:55 -0500

    index page create

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -1,7 +1,13 @@
 class CharactersController < ApplicationController
   def index
     @characters = Character.all
+    render "index.json.jbuilder"
   end
   def show
+    @character = Character.find_by(id: params[:id])
+    render "show.json.jbuilder"
   end
+  def create
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   get "/characters" => "characters#index"
+  post "/characters" => "characters#create"
   get "/characters/:id" => "characters#show"
 end
