ec5b10d6834c93b8b1288135568108aad220fea1
lauriemcgee <livingoncoffeeandflowers@gmail.com>
2017-05-15 15:37:45 -0500

    added authentication user model for API

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :null_session
-  
+  before_action :restrict_access
+
+  def restrict_access
+    authenticate_or_request_with_http_token do |api_key, options|
+      if User.findy_by(email: request.headers['X-User-Email'], api_key: api_key)
+        true
+      else
+        false
+      end
+    end
+  end
 end
